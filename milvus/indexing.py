ac569e9e46bd6801491fa0d72337ad2e94b77ec5
Ashwin Hegde <ashegde@paypal.com>
2024-03-31 12:05:09 +0530

    add(eg): indexing type

--- milvus/indexing.py
+++ milvus/indexing.py
@@ -13,11 +13,57 @@ print(utility.list_collections())
 
 collection = Collection("album1")
 
+# Flat
 index_params = {
   "metric_type": "L2",
-  "index_type": "IVF_FLAT",
-  "params": {"nlist": 1024}
+  "index_type": "FLAT",
+  "params": {},
+  "index_name": "FLAT_INDEX_EG"
 }
+
+# IVF_FLAT
+# index_params = {
+#   "metric_type": "L2",
+#   "index_type": "IVF_FLAT",
+#   "params": {"nlist":1024},
+#   "index_name": "IVF_FLAT_INDEX_EG"
+# }
+
+# During search and query
+# search_params = {"metric_type": "L2", "params": {"nprobe": 64}}
+
+# IVF_SQ8
+# index_params = {
+#   "metric_type": "L2",
+#   "index_type": "IVF_SQ8",
+#   "params": {"nlist":1024},
+#   "index_name": "IVF_SQ8_INDEX_EG"
+# }
+
+# During search and query
+# search_params = {"metric_type": "L2", "params": {"nprobe": 64}}
+
+# IVF_PQ
+# index_params = {
+#   "metric_type": "L2",
+#   "index_type": "IVF_PQ",
+#   "params": {"nlist":1024, "m": 2, "nbits": 16},
+#   "index_name": "IVF_PQ_INDEX_EG"
+# }
+
+# During search and query
+# search_params = {"metric_type": "L2", "params": {"nprobe": 64}}
+
+# HNSW
+# index_params = {
+#   "metric_type": "L2",
+#   "index_type": "HNSW",
+#   "params": {"M": 2, "efConstruction": 16},
+#   "index_name": "HNSW_INDEX_EG"
+# }
+
+# During search and query
+# search_params = {"metric_type": "L2", "params": {"ef": 4}}
 
 collection.create_index(
   field_name="song_vec",
